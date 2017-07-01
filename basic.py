77e77bc16151fee481225ecd83a6828c325249b0
ethender <ethender.9966526462@gmail.com>
2017-07-01 10:08:13 +0530

    some learn things

---
+++ basic.py
@@ -0,0 +1,17 @@
+tuple1 = (1,2,3,4,5) ## immutable
+list1 = [1,2,3,4,5] ## mutable
+dict1 = {'one':1,'two':2,'three':3,'four':4,'five':5}
+
+
+print type(tuple1)
+print type(list1)
+print type(dict1)
+
+
+
+##
+##
+## cloning
+##
+
+tuple2 = tuple1[:]
