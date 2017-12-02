3c3ddaf5dcb51388efd2882410687bc01d76569c
ethender <ethender.9966526462@gmail.com>
2017-12-02 17:35:27 +0530

    lec 18

---
+++ Lectures/Lecture18/lec18.py
@@ -0,0 +1,103 @@
+"""
+
+Lec 18 : Optimization Problems and Algorithms
+
+"""
+"""
+
+r**2 == 1 : Model is explains all varialbility of data
+r**0 == 0 : no linear relationship of data
+
+
+Compute Speed of  arrow:
+1. Trajectory given by, y = ax**2+bx+c
+    Peak of parabola occurs halfway between launch and target, call this xMild
+
+    yPeak =x*xMid**2 + b*xMid+c
+
+2. Time to fall from yPeak to target (height = 0),
+    purely a function of acceleration due to gravity.
+
+    t= sqrt(2*yPeak/g)
+    g = gravity
+3. This is also the time required to go from xMid to xMax.
+    Can easily compute the average horizontal speed over that distance.
+    If we assume no drag, that speed is horizontal speed at which projectile hits target.
+"""
+
+import pylab
+
+
+def rSquare(measured, estimated):
+    """measured: one dimensional array of measured values
+       estimate: one dimensional array of predicted values"""
+    EE = ((estimated - measured)**2).sum()
+    mMean = measured.sum()/float(len(measured))
+    MV = ((mMean - measured)**2).sum()
+    return 1 - EE/MV
+
+def getTrajectoryData(fileName):
+    dataFile = open(fileName, 'r')
+    distances = []
+    heights1, heights2, heights3, heights4 = [],[],[],[]
+    discardHeader = dataFile.readline()
+    for line in dataFile:
+        d, h1, h2, h3, h4 = line.split()
+        distances.append(float(d))
+        heights1.append(float(h1))
+        heights2.append(float(h2))
+        heights3.append(float(h3))
+        heights4.append(float(h4))
+    dataFile.close()
+    return (distances, [heights1, heights2, heights3, heights4])
+
+
+def getXSpeed(a,b,c,minX,maxX):
+    xMid = (maxX - minX)/2.0
+    yPeak = a*xMid**2 + b*xMid + c
+    g = 32.16*12 #accel of gravity in inches/sec/sec
+    t = (2.0*yPeak/g)**0.5
+    return xMid/(t*12.0)
+
+def processTrajectories(fName):
+    distances, heights = getTrajectoryData(fName)
+    distances = pylab.array(distances)*36
+    totHeights = pylab.array([0]*len(distances))
+    for h in heights:
+        totHeights = totHeights + pylab.array(h)
+    pylab.title('Trajectory of Projectile (Mean of 4 Trials)')
+    pylab.xlabel('Inches from Launch Point')
+    pylab.ylabel('Inches Above Launch Point')
+    meanHeights = totHeights/len(heights)
+    pylab.plot(distances, meanHeights, 'bo')
+    a,b,c = pylab.polyfit(distances, meanHeights, 2)
+    altitudes = a*(distances**2) +  b*distances + c
+    speed = getXSpeed(a, b, c, distances[-1], distances[0])
+    pylab.plot(distances, altitudes, 'g',
+               label = 'Quad. Fit' + ', R2 = '
+               + str(round(rSquare(meanHeights, altitudes), 2))
+               + ', Speed = ' + str(round(speed, 2)) + 'feet/sec')
+    pylab.legend()
+processTrajectories('launcher_data.txt')
+pylab.show()
+
+
+
+
+"""
+1. start with an experiment
+2. Use computation to find and evaluate 
+3. Used some theory + analysis + computation
+to derive a consequence of model
+
+
+
+Optimization Problems
+ 1. An Objective Function
+ 2. A set of constraints
+
+ Problem reduction
+"""
+
+
+    
